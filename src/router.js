40f4de49a10301f82082c88b9d1e7c4deb8a8dd0
downthecrop <bryce@mathx.ca>
2023-01-11 10:23:11 -0800

    convert basepath to contant

--- src/router.js
+++ src/router.js
@@ -2,9 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Chat from './views/Chat.vue'
 import Home from './views/Home.vue'
 
+const baseURL = '/routes-sandbox/'
+
 export const router = createRouter({
-  base: '/routes-sandbox/',
+  base: baseURL,
   history: createWebHistory(),
-  routes: [{ path: '/:username', component: Chat },
-  { path: '/', component: Home }],
+  routes: [
+    { path: '/', component: Home },
+    { path: baseURL+'/:username', component: Chat },
+    { path: baseURL, component: Home },
+  ],
 })
