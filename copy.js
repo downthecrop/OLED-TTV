a944df59078c35b7940c243e5fd25b4cde01f789
downthecrop <bryce@mathx.ca>
2023-08-17 20:12:01 -0700

    Auto create the 404.html page (required for vue-router on ghpages)

---
+++ copy.js
@@ -0,0 +1,9 @@
+﻿const fs = require('fs');
+
+const source = 'dist/index.html';
+const destination = 'dist/404.html';
+
+fs.copyFile(source, destination, (err) => {
+  if (err) throw err;
+  console.log('index.html was copied to 404.html');
+});
